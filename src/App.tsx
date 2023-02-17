f2413ada8e24261f783448e9bd0ac815c4fceea5
Fanki <domi.fankhauser@hotmail.com>
2023-02-17 14:06:30 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,51 @@
-import { Fragment, useState, useMemo } from 'react';
-import { JsonForms } from '@jsonforms/react';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import logo from './logo.svg';
-import './App.css';
-import schema from './schema.json';
-import uischema from './uischema.json';
-import {
-  materialCells,
-  materialRenderers,
-} from '@jsonforms/material-renderers';
-import RatingControl from './RatingControl';
-import ratingControlTester from './ratingControlTester';
-import { makeStyles } from '@mui/styles';
+import { Fragment, useState, useMemo } from "react";
+import { JsonForms } from "@jsonforms/react";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import logo from "./logo.svg";
+import "./App.css";
+import schema from "./schema.json";
+import uischema from "./uischema.json";
+import { materialCells, materialRenderers } from "@jsonforms/material-renderers";
+import RatingControl from "./RatingControl";
+import ratingControlTester from "./ratingControlTester";
+import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles({
   container: {
-    padding: '1em',
-    width: '100%',
+    padding: "1em",
+    width: "100%",
   },
   title: {
-    textAlign: 'center',
-    padding: '0.25em',
+    textAlign: "center",
+    padding: "0.25em",
   },
   dataContent: {
-    display: 'flex',
-    justifyContent: 'center',
-    borderRadius: '0.25em',
-    backgroundColor: '#cecece',
-    marginBottom: '1rem',
+    display: "flex",
+    justifyContent: "center",
+    borderRadius: "0.25em",
+    backgroundColor: "#cecece",
+    marginBottom: "1rem",
   },
   resetButton: {
-    margin: 'auto !important',
-    display: 'block !important',
+    margin: "auto !important",
+    display: "block !important",
   },
   demoform: {
-    margin: 'auto',
-    padding: '1rem',
+    margin: "auto",
+    padding: "1rem",
   },
 });
 
 const initialData = {
-  name: 'Send email to Adrian',
-  description: 'Confirm if you have passed the subject\nHereby ...',
-  done: true,
-  recurrence: 'Daily',
-  rating: 3,
+  orders: [
+    {
+      title: "42 killer robots",
+      ordered: true,
+      assignee: "Philip J. Fry",
+    },
+  ],
 };
 
 const renderers = [
@@ -66,38 +65,33 @@ const App = () => {
 
   return (
     <Fragment>
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to JSON Forms with React</h1>
-          <p className='App-intro'>More Forms. Less Code.</p>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to JSON Forms with React</h1>
+          <p className="App-intro">More Forms. Less Code.</p>
         </header>
       </div>
 
-      <Grid
-        container
-        justifyContent={'center'}
-        spacing={1}
-        className={classes.container}
-      >
+      <Grid container justifyContent={"center"} spacing={1} className={classes.container}>
         <Grid item sm={6}>
-          <Typography variant={'h4'} className={classes.title}>
+          <Typography variant={"h4"} className={classes.title}>
             Bound data
           </Typography>
           <div className={classes.dataContent}>
-            <pre id='boundData'>{stringifiedData}</pre>
+            <pre id="boundData">{stringifiedData}</pre>
           </div>
           <Button
             className={classes.resetButton}
             onClick={clearData}
-            color='primary'
-            variant='contained'
+            color="primary"
+            variant="contained"
           >
             Clear data
           </Button>
         </Grid>
         <Grid item sm={6}>
-          <Typography variant={'h4'} className={classes.title}>
+          <Typography variant={"h4"} className={classes.title}>
             Rendered form
           </Typography>
           <div className={classes.demoform}>
